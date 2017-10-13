8cb1b8fc84cee98afbffed4f198d9affabcf3775
Adam Fudge <coolfudge@gmail.com>
2017-10-13 21:13:39 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    if self[self.size - 1]
+    if self[-1]
   end
 
   def question?
-    if self[self.size - 1]
+    if self[-1]
   end
 
   def exclamation?
-    if self[self.size - 1]
+    if self[-1]
   end
 
   def count_sentences
